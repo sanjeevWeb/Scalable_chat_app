63e2465bf115dd21a0b0a902b1c81beba6395a39
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-04-19 10:16:44 +0530

    fixed socket and kafka error

--- backend/src/utility/services/websoket/socket.ts
+++ backend/src/utility/services/websoket/socket.ts
@@ -1,6 +1,7 @@
 import { Server, Socket } from "socket.io";
 import jwtAuth, { TokenPayload } from "utility/hash/jwtAuth.js";
 import { Redis } from 'ioredis'
+import { produceMessages } from "../kafka/kafka.js";
 
 interface CustomSocket extends Socket {
     userData?: TokenPayload | any;
@@ -24,13 +25,13 @@ export function setupSocket(io: Server) {
         // * Join the room
         socket.join(socket.userData);
 
-        socket.on("message", async (data) => {
+        socket.on("message", async (data: any) => {
           try {
-            await produceMessage("chats", data);
+            await produceMessages(data);
           } catch (error) {
             console.log("The kafka produce error is", error);
           }
-          socket.to(socket.room).emit("message", data);
+          // socket.to(socket.room).emit("message", data);
         });
 
         socket.on("disconnect", () => {
