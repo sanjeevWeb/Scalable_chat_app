54cd77b725072343552de7deb35b9f2641bbf3c1
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-04-26 09:51:02 +0530

    modified socket connection logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,7 @@ import { createServer } from 'http';
 import { Server } from "socket.io";
 import { publisher, setupSocket, subscriber } from "utility/services/websoket/socket.js";
 import { createAdapter} from '@socket.io/redis-adapter'
-// import logger from "./logger/logger";
-// import morgan from "morgan"
 
-const morganFormat = ":method :url :status :response-time ms";
 dotenv.config()
 
 const app:Application = express();
@@ -30,21 +27,7 @@ app.use((req, res, next) => {
   console.log(`${timestamp} - ${req.method} ${req.originalUrl}`);
   next();
 });
-// app.use(
-//     morgan(morganFormat, {
-//       stream: {
-//         write: (message:String) => {
-//           const logObject = {
-//             method: message.split(" ")[0],
-//             url: message.split(" ")[1],
-//             status: message.split(" ")[2],
-//             responseTime: message.split(" ")[3],
-//           };
-//           logger.info(JSON.stringify(logObject));
-//         },
-//       },
-//     })
-//   );
+
 
 const server = createServer(app); //http server
 const io = new Server(server, {
--- backend/src/utility/services/websoket/socket.ts
+++ backend/src/utility/services/websoket/socket.ts
@@ -4,38 +4,47 @@ import { Redis } from 'ioredis'
 import { produceMessages } from "../kafka/kafka.js";
 
 interface CustomSocket extends Socket {
-    userData?: TokenPayload | any;
+  userData?: TokenPayload | any;
 }
 
 export const publisher = new Redis(); // defaults to localhost:6379
 export const subscriber = new Redis();
 
 export function setupSocket(io: Server) {
-    io.use((socket: CustomSocket, next) => {
-        const token: string = socket.handshake.auth.token || socket.handshake.headers.token;
-        if (!token) {
-            return next(new Error("Invalid token"));
-        }
-        const userData = jwtAuth.verifyAccessToken(token)
-        socket.userData = userData;
-        next();
+  io.use((socket: CustomSocket, next) => {
+    const token: string = socket.handshake.auth.token || socket.handshake.headers.token;
+    if (!token) {
+      return next(new Error("Invalid token"));
+    }
+    const userData = jwtAuth.verifyAccessToken(token)
+    socket.userData = userData;
+    next();
+  });
+
+  io.on("connection", async (socket: CustomSocket) => {
+    // * Join the room
+    const userId = socket.userData?.user_id;
+
+    if (!userId) return socket.disconnect();
+
+    // Store mapping in Redis
+    await publisher.set(`user:${userId}`, socket.id);
+    socket.join(`user:${userId}`); // allows targeted emits by room
+
+
+    socket.on("message", async (data: any) => {
+      try {
+        await produceMessages(data);
+      }
+      catch (error) {
+        console.log("The kafka produce error is", error);
+      }
+      // socket.to(socket.room).emit("message", data);
     });
 
-    io.on("connection", (socket: CustomSocket) => {
-        // * Join the room
-        socket.join(socket.userData);
-
-        socket.on("message", async (data: any) => {
-          try {
-            await produceMessages(data);
-          } catch (error) {
-            console.log("The kafka produce error is", error);
-          }
-          // socket.to(socket.room).emit("message", data);
-        });
-
-        socket.on("disconnect", () => {
-            console.log("A user disconnected:", socket.id);
-        });
+    socket.on("disconnect", async () => {
+      await publisher.del(`user:${userId}`);
+      console.log("A user disconnected:", socket.id);
     });
+  });
 }
